fcd84473c110bfa6b83b4530b64cca8b21164b49
Monsif El Ouarat <monsifelouarat@gmail.com>
2024-02-04 16:53:12 +0100

    Challenges

---
+++ day1/processFiles.js
@@ -0,0 +1,34 @@
+const fs = require("fs");
+const readFile = require("./readFileAsync");
+const writeFile = require("./writeFileAsync");
+
+//
+async function processFiles() {
+  try {
+    // get Files from Current Folder
+    const dirFiles = fs.readdirSync("./");
+
+    //Filtered Files from dirFile that end with .txt
+    const filtered = dirFiles.filter((file) => file.endsWith(".txt"));
+
+    console.log("filtered : ", filtered);
+    console.log("dirFiles :", dirFiles);
+
+    // Loop the Filtred files
+    for (let i = 0; i < filtered.length; i++) {
+      // read the file
+      const content = await readFile.readFileAsync(`./${filtered[i]}`);
+      // console.log(content);
+      // Manipulate contenct to upper case
+      const upper = content.toUpperCase();
+      // log
+      console.log("Upper case :", upper);
+      writeFile.writeFileAsync(`./${"upper " + filtered[i]}`, upper);
+    }
+    return "Ok";
+  } catch (error) {
+    return error;
+  }
+}
+
+module.exports = { processFiles };
